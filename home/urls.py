91fb2587a74e4e8bc95e677ab2706590611d998d
NestaNdereva23 <nestandereva@gmail.com>
2025-02-25 23:12:19 +0300

    Email configuration setup and fixes
    - added django-gmail setup and environment variables for reseting passwords
    - fixed password_reset url  not loading custom templates
    - fixed password_reset done page removed extends base page to extend the navbar

--- home/urls.py
+++ home/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
 
     path('login/password_reset/', auth_views.PasswordResetView.as_view(template_name="registration/password_reset_form.html"), name="password_reset"),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_done.html"), name="password_reset_done"),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+
 
 ]
